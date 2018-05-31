23985db6bd1094413550e135d0e0db5a71cf80c2
MorganDawe <mdawe@discoverygarden.ca>
2018-05-31 11:30:11 -0300

    Updating styles and templatest, works around private modules.

---
+++ templates/islandora-sp/islandora-objects.tpl.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @file
+ * Render a bunch of objects in a list or grid view.
+ */
+?>
+<div class="islandora-objects clearfix">
+  <?php print $pager; ?>
+  <?php print $content; ?>
+  <?php print $pager; ?>
+</div>
